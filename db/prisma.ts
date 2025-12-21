c7ce9787639b9701a61764e5dfb76981798f0d33
RuslanIman-Aliev <s0970802047@gmail.com>
2025-12-21 15:04:46 +0100

    Initial deploy commit

---
+++ db/prisma.ts
@@ -0,0 +1,44 @@
+import { neonConfig } from '@neondatabase/serverless';
+import { PrismaNeon } from '@prisma/adapter-neon';
+import { PrismaClient } from '@prisma/client';
+import ws from 'ws';
+
+// 1. Настраиваем WebSocket (Это обязательно для Neon Serverless!)
+neonConfig.webSocketConstructor = ws;
+
+// 2. Ваша строка подключения
+// Я оставил её здесь, чтобы исключить проблему с нечитаемым .env
+const connectionString = "postgresql://neondb_owner:npg_mNtAsJFh5g0v@ep-purple-base-adlsfhid-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require";
+
+// 3. НОВЫЙ ПОДХОД (как на скриншоте)
+// Мы больше не создаем "new Pool()". Мы передаем настройки сразу в адаптер.
+const adapter = new PrismaNeon({
+  connectionString: connectionString
+});
+
+// 4. Паттерн Singleton для Next.js (чтобы не плодить соединения)
+const globalForPrisma = global as unknown as { prisma: any };
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter, // Теперь адаптер совместим без "as any"
+    log: ['error'],
+  }).$extends({
+    result: {
+      product: {
+        price: {
+          compute(product) {
+            return product.price.toString();
+          },
+        },
+        rating: {
+          compute(product) {
+            return product.rating.toString();
+          },
+        },
+      },
+    },
+  });
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
